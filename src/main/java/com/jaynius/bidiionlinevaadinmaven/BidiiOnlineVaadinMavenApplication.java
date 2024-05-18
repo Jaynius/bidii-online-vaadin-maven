5a0f4e3bc2415f110493bab170bd885cead34a48
bram <bramweljumab@gmail.com>
2024-05-18 11:33:42 +0300

    bootstrap project

---
+++ src/main/java/com/jaynius/bidiionlinevaadinmaven/BidiiOnlineVaadinMavenApplication.java
@@ -0,0 +1,13 @@
+package com.jaynius.bidiionlinevaadinmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidiiOnlineVaadinMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidiiOnlineVaadinMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaynius/bidiionlinevaadinmaven/BidiiOnlineVaadinMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaynius.bidiionlinevaadinmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidiiOnlineVaadinMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
